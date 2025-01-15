8c2a07c59344e99c19c52b154ba6006f8b3c1f80
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-15 21:18:49 +0100

    feature: simplify lint (#5)
    * feature: simplify lint
    
    * add dependencies
    
    * add assert
    
    * keyword
    
    * simplify rpc
    
    * skip test for now
    
    * remove unused
    
    * skip test
    
    * simplify code

--- packages/debug-view/test/ParentRpc.test.ts
+++ packages/debug-view/test/ParentRpc.test.ts
@@ -1,17 +1,19 @@
 import { beforeEach, expect, jest, test } from '@jest/globals'
 import * as ParentRpc from '../src/parts/ParentRpc/ParentRpc.ts'
+import * as RpcId from '../src/parts/RpcId/RpcId.ts'
+import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
 
 beforeEach(() => {
   jest.resetAllMocks()
 })
 
-const mockRpc: any = {
+const mockRpc = {
   invoke: jest.fn(),
-}
+} as any
 
 test('invoke - calls rpc.invoke with correct arguments', async () => {
   mockRpc.invoke.mockResolvedValue(42)
-  ParentRpc.setRpc(mockRpc)
+  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
   // @ts-ignore
   const result = await ParentRpc.invoke('test.method', 1, 'abc')
   expect(mockRpc.invoke).toHaveBeenCalledWith('test.method', 1, 'abc')
@@ -20,19 +22,14 @@ test('invoke - calls rpc.invoke with correct arguments', async () => {
 
 test('invoke - handles error from rpc', async () => {
   mockRpc.invoke.mockRejectedValue(new Error('test error'))
-  ParentRpc.setRpc(mockRpc)
+  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
   // @ts-ignore
   await expect(ParentRpc.invoke('test.method')).rejects.toThrow('test error')
 })
 
 test('invoke - throws if rpc is not set', () => {
-  ParentRpc.setRpc(undefined)
+  // @ts-ignore
+  RpcRegistry.set(RpcId.RendererWorker, undefined)
   // @ts-ignore
   expect(() => ParentRpc.invoke('test.method')).toThrow()
-})
-
-test('setRpc - sets rpc instance', () => {
-  ParentRpc.setRpc(mockRpc)
-  // @ts-ignore
-  expect(() => ParentRpc.invoke('test')).not.toThrow()
 })
