ff9ffd620463a20e390014353a2ea81ec79220d1
Le Vivilet <levivlet@pm.me>
2025-01-15 21:00:25 +0100

    step out

--- packages/debug-view/src/parts/CommandMap/CommandMap.ts
+++ packages/debug-view/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
 import * as HandleClickPauseOnUncaughtExceptions from '../HandleClickPauseOnUncaughtExceptions/HandleClickPauseOnCaughtExceptions.ts'
 import * as HandleClickSectionWatch from '../HandleClickSectionWatch/HandleClickSectionWatch.ts'
+import * as StepOut from '../StepOut/StepOut.ts'
 
 export const commandMap = {
   'Debug.handleClickPauseOnUncaughtExceptions': HandleClickPauseOnUncaughtExceptions.handleClickPauseOnUncaughtExceptions,
   'Debug.handleClickSectionWatch': HandleClickSectionWatch.handleClickSectionWatch,
+  'Debug.stepOut': StepOut.stepOut,
 }
