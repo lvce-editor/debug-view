95c1057bda243436ef50216c870c84b24cffaa07
Le Vivilet <levivlet@pm.me>
2025-01-15 21:05:48 +0100

    add step

--- packages/debug-view/src/parts/CommandMap/CommandMap.ts
+++ packages/debug-view/src/parts/CommandMap/CommandMap.ts
@@ -2,10 +2,12 @@ import * as HandleClickPauseOnUncaughtExceptions from '../HandleClickPauseOnUnca
 import * as HandleClickSectionWatch from '../HandleClickSectionWatch/HandleClickSectionWatch.ts'
 import * as StepInto from '../StepInto/StepInto.ts'
 import * as StepOut from '../StepOut/StepOut.ts'
+import * as StepOver from '../StepOver/StepOver.ts'
 
 export const commandMap = {
   'Debug.handleClickPauseOnUncaughtExceptions': HandleClickPauseOnUncaughtExceptions.handleClickPauseOnUncaughtExceptions,
   'Debug.handleClickSectionWatch': HandleClickSectionWatch.handleClickSectionWatch,
   'Debug.stepOut': StepOut.stepOut,
   'Debug.stepInto': StepInto.stepInto,
+  'Debug.stepOver': StepOver.stepOver,
 }
