0e62c5bff44cca0ef361a5e12106eec544a6c864
Le Vivilet <levivlet@pm.me>
2025-01-15 20:55:54 +0100

    use section

--- packages/debug-view/src/parts/CommandMap/CommandMap.ts
+++ packages/debug-view/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
 import * as HandleClickPauseOnUncaughtExceptions from '../HandleClickPauseOnUncaughtExceptions/HandleClickPauseOnCaughtExceptions.ts'
+import * as HandleClickSectionWatch from '../HandleClickSectionWatch/HandleClickSectionWatch.ts'
 
 export const commandMap = {
   'Debug.handleClickPauseOnUncaughtExceptions': HandleClickPauseOnUncaughtExceptions.handleClickPauseOnUncaughtExceptions,
+  'Debug.handleClickSectionWatch': HandleClickSectionWatch.handleClickSectionWatch,
 }
