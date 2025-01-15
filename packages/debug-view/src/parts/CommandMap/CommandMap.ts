ae7c75eed02f5c3d214115e292406b2932df9e24
Le Vivilet <levivlet@pm.me>
2025-01-15 20:39:48 +0100

    feature: add handleClickPauseOnUncaughtExceptions

--- packages/debug-view/src/parts/CommandMap/CommandMap.ts
+++ packages/debug-view/src/parts/CommandMap/CommandMap.ts
@@ -1,1 +1,5 @@
-export const commandMap = {}
+import * as HandleClickPauseOnUncaughtExceptions from '../HandleClickPauseOnUncaughtExceptions/HandleClickPauseOnCaughtExceptions.ts'
+
+export const commandMap = {
+  'Debug.handleClickPauseOnUncaughtExceptions': HandleClickPauseOnUncaughtExceptions.handleClickPauseOnUncaughtExceptions,
+}
